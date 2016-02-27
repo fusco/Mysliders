29be7149930a4ba79dcb58916c55cd9e90ac7cfc
David Demonchy <fusco.spv@gmail.com>
2016-02-27 01:31:29 +0100

    ajout des routes et du namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,57 +1,27 @@
 Rails.application.routes.draw do
-  resources :slides
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+# pour la resource slides sur le frontend,
+# on autorise que la vue index
+# les autres vues ne seront pas utilisées
+  resources :slides, only: [:index]
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # on crée un namespace admin pour administrer les slides
+  namespace :admin do
+    resources :slides
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    # On ajoute un root dans le namespace pour que
+    # http://localhost:3000/admin
+    # charge par défaut
+    # http://localhost:3000/admin/sliders
+    # l'url dans la barre d'adresse restera
+    # http://localhost:3000/admin
+    root 'slides#index'
+  end
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  # On crée là aussi un root pour que
+  # http://localhost:3000
+  # charge http://localhost:3000/sliders
+  # l'url dans la barre d'adresse restera
+  # http://localhost:3000/
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  root 'slides#index'
 end
